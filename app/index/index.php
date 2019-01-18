d41b66eef8dc0f297bcee2ec36f32929dce4668b
Mohammad_Ayoub <mohammadayoub747@gmail.com>
2019-01-18 18:03:53 +0500

    Widgets Done on Index Page

--- app/index/index.php
+++ app/index/index.php
@@ -4,14 +4,7 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>
-        Home 
-        <!-- <small>Control panel</small> -->
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="../../#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
+     
 
     </section>
 
@@ -94,31 +87,7 @@
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-             <?php
-                $sql = "Select COUNT(group_id) AS Tot FROM groups ";
-                $res = mysqli_query($con,$sql);
-                while($r = mysqli_fetch_array($res))
-                {
-
-                  $c = $r["Tot"];
-                }
-              ?>
-              <h3><?php echo $c ?></h3>
-              <p>New Groups</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-groups"></i>
-            </div>
-            <a href="../groups/index.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-      <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
+          <!--  -->
           <div class="small-box" style="background-color: #3c8dbc;">
             <div class="inner">
              <?php
@@ -137,6 +106,31 @@
               <i class="ion ion-pie-graph"></i>
             </div>
             <a href="../class/index.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+             <?php
+                $sql = "Select COUNT(group_id) AS Tot FROM groups ";
+                $res = mysqli_query($con,$sql);
+                while($r = mysqli_fetch_array($res))
+                {
+
+                  $c = $r["Tot"];
+                }
+              ?>
+              <h3><?php echo $c ?></h3>
+              <p>New Groups</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-groups"></i>
+            </div>
+            <a href="../groups/index.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
       </div>
